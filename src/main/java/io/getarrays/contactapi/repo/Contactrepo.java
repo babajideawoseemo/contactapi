310f55b93706a61e36d39241590ab964511d384a
babajideawoseemo <akinloluolaposi@gmail.com>
2024-04-08 13:59:35 +0100

    Contact API Backend with Java 21 and Postgresql

---
+++ src/main/java/io/getarrays/contactapi/repo/Contactrepo.java
@@ -0,0 +1,13 @@
+package io.getarrays.contactapi.repo;
+
+import io.getarrays.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface Contactrepo extends JpaRepository<Contact, String> {
+    @Override
+    Optional<Contact> findById(String id);
+}
